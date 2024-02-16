b26d42a2b10909de5d45cb3a0860b6b88140b606
sky line <32512284+linesky@users.noreply.github.com>
2024-02-16 20:25:49 +0000

    Add files via upload

---
+++ kernel.asm
@@ -0,0 +1,24 @@
+global kernel_main
+extern clear
+extern print
+extern locate
+
+kernel_main:
+call clear
+
+mov eax,10
+push eax
+push eax
+call locate
+pop eax
+pop eax
+mov eax,hello
+push eax
+call print
+pop eax
+ret
+
+
+
+
+hello db "hello world...",0
